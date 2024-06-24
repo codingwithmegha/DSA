3a77bcab91187a1941c959d78f21f1b8200fce6b
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2024-06-24 19:53:30 +0530

    Add files via upload

---
+++ matrixMul.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int i,j,a[2][2],b[2][2],k;
+    int c[2][2];  
+	printf("Enter the 1st array:");
+	  for(i=0; i<2; i++)
+	  {
+	     for(j=0; j<2; j++)
+	       {
+     	      scanf("%d",&a[i][j]);
+           }
+       }
+              printf("Enter the 2st array:");
+	              for(i=0; i<2; i++)
+	                {
+					  for(j=0; j<2; j++)
+	                  {
+                      	scanf("%d",&b[i][j]);
+                      }
+                  }
+                      for(i=0; i<2; i++)
+                      {
+	                 for(j=0; j<2; j++)
+	                 {
+	                 	c[i][j]=0;
+	                 	for(k=0; k<2; k++)
+						 {
+                        c[i][j]=c[i][j]+a[i][k]*b[k][j];
+                    }
+                }
+            }
+                         printf("multiplication of array:");
+                                 for(i=0; i<2; i++)
+	                         for(j=0; j<2; j++)
+	                             {
+     	                             printf("%d\n",c[i][j]);
+                                 }
+getch();
+}
